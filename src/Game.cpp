f3075f90ff816adb1874875e55cd2b05616127b3
Berlin <evan1234.ek@gmail.com>
2026-04-26 00:58:30 +0600

    separate physics from game core

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,10 +9,6 @@
  * @brief Construct a new Game:: Game object
  */
 Game::Game() {
-  // initialize all keys to false
-  for (int i = 0; i < 256; i++) {
-    keyPressed[i] = false;
-  }
 }
 
 /**
@@ -32,118 +28,27 @@ void Game::init() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 
-  tiles = {
-      {0, GROUND_Y, 900, GROUND_TILE_HEIGHT, 0.35f, 0.65f, 0.25f},
-      {1000, GROUND_Y, 900, GROUND_TILE_HEIGHT, 0.35f, 0.65f, 0.25f},
-
-      // random tiles
-      {250, 120, 180, 20, 0.55f, 0.40f, 0.25f},
-      {550, 220, 180, 20, 0.55f, 0.40f, 0.25f},
-      {820, 320, 180, 20, 0.55f, 0.40f, 0.25f},
-      {1100, 180, 180, 20, 0.55f, 0.40f, 0.25f},
-      {400, 340, 120, 20, 0.70f, 0.30f, 0.30f},
-      {720, 20, 20, 100, 0.30f, 0.30f, 0.70f},
-  };
+  // load level datas
+  level.loadTest();
 }
 
 /**
- * @brief Resolve collision on the X-axis.
- *
- * @param tile The tile to check collision with.
- */
-void Game::resolveCollisionX(Tile& tile) {
-  Rect playerRect = player.getRect();
-  Rect tileRect   = tile.getRect();
-
-  // no collision, do nothing
-  if (!playerRect.overlaps(tileRect)) {
-    return;
-  }
-
-  float overlapX = playerRect.overlapX(tileRect);
-
-  // by measure center of player and tile. we can get which direction player comes from and push the
-  // player to the direction.
-  if (playerRect.centerX() < tileRect.centerX()) {
-    player.x -= overlapX;
-  } else {
-    player.x += overlapX;
-  }
-
-  player.vx = 0;
-}
-
-/**
- * @brief Resolve collision on the Y-axis.
- *
- * @param tile The tile to check collision with.
- */
-void Game::resolveCollisionY(Tile& tile) {
-  Rect playerRect = player.getRect();
-  Rect tileRect   = tile.getRect();
-
-  // no collision, do nothing
-  if (!playerRect.overlaps(tileRect)) {
-    return;
-  }
-
-  float overlapY = playerRect.overlapY(tileRect);
-  if (playerRect.centerY() > tileRect.centerY()) {
-    player.y += overlapY;
-    player.vy       = 0;
-    player.onGround = true;
-  } else {
-    player.y -= overlapY;
-    player.vy = 0;
-  }
-
-  // player.vy = 0;
-}
-
-/**
- * @brief Update the game state. Serial by handle input, physics, collision and camera update.
- *  This called in every frame by main loop.
+ * @brief Update the game state every frame.
  *
  * @param deltaTime Time elapsed since the last update(in seconds).
  */
 void Game::update(float deltaTime) {
   // key press handling checked in every upate for smooth movement
-  if (keyPressed['a'] or keyPressed['A']) {
+  if (input.isHeld('a') || input.isHeld('A')) {
     player.moveLeft();
-  } else if ((keyPressed['d'] or keyPressed['D'])) {
+  } else if (input.isHeld('d') || input.isHeld('D')) {
     player.moveRight();
   } else {
     player.stopMoving();
   }
 
-  // applied gravity to player.
-  player.applyGravity(deltaTime);
-
-  // ------ X axis movement and collision ------
-  // move x axis only
-  player.x += player.vx * deltaTime;
-
-  // edges of world
-  if (player.x < 0) {
-    player.x  = 0;
-    player.vx = 0;
-  }
-
-  // resolve x-axis collision with tiles
-  for (auto& tile : tiles) {
-    resolveCollisionX(tile);
-  }
-
-  // ------ Y axis movement and collision ------
-  // move y axis only
-  player.y += player.vy * deltaTime;
-
-  player.onGround = false;  // reset ground before collision check
-
-  // resolve y-axis collision with tiles
-  for (auto& tile : tiles) {
-    resolveCollisionY(tile);
-  }
+  // update physics of player and resolve collision with tiles
+  physics.update(player, level.tiles, deltaTime);
 
   // camera follows the player
   camera.update(player.x, player.y);
@@ -158,10 +63,7 @@ void Game::render() {
 
   camera.applyTransform();
 
-  // render tile in world coordinate
-  for (auto& tile : tiles) {
-    tile.render();
-  }
+  level.render();  // render tiles in world coordinate
 
   player.render();  // render in world coordinate
 
@@ -177,7 +79,8 @@ void Game::render() {
  * @params x, y = mouse position.
  */
 void Game::handleKeyDown(unsigned char key, int x, int y) {
-  keyPressed[key] = true;
+  input.keyDown(key);
+
   if (key == ' ') {
     player.jump();
   }
@@ -193,5 +96,5 @@ void Game::handleKeyDown(unsigned char key, int x, int y) {
  * @params x, y = mouse position.
  */
 void Game::handleKeyUp(unsigned char key, int x, int y) {
-  keyPressed[key] = false;
+  input.keyUp(key);
 }
