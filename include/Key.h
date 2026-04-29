553c7814bfd696ec709c4ac9c6bb089e9ecd2635
Berlin <evan1234.ek@gmail.com>
2026-04-30 02:58:52 +0600

    feat(key): add key object

---
+++ include/Key.h
@@ -0,0 +1,18 @@
+#ifndef KEY_H
+#define KEY_H
+
+#include "Rect.h"
+
+/**
+ * @struct Key
+ * @brief a collectible key object
+ */
+struct Key {
+  float x, y, width, height;
+  bool  collected = false;
+
+  Rect getRect() const;
+  void render() const;
+};
+
+#endif  // KEY_H
---
+++ src/Key.cpp
@@ -0,0 +1,29 @@
+#include "Key.h"
+
+#include "Platform.h"
+
+Rect Key::getRect() const {
+  return {x, y, width, height};
+}
+
+void Key::render() const {
+  if (collected) {
+    return;
+  }
+
+  glColor3f(1.0f, 0.85f, 0.0f);
+  glBegin(GL_QUADS);
+  glVertex2f(x, y);
+  glVertex2f(x + width, y);
+  glVertex2f(x + width, y + height);
+  glVertex2f(x, y + height);
+  glEnd();
+
+  glColor3f(0.7f, 0.55f, 0.0f);
+  glBegin(GL_LINE_LOOP);
+  glVertex2f(x, y);
+  glVertex2f(x + width, y);
+  glVertex2f(x + width, y + height);
+  glVertex2f(x, y + height);
+  glEnd();
+}
