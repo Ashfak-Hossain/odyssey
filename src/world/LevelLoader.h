cece49480b9fb654a83b2af13c34e7fda2670c35
Berlin <evan1234.ek@gmail.com>
2026-05-03 22:36:51 +0600

    reafctor(levelLoader): refactor comments

--- src/world/LevelLoader.h
+++ src/world/LevelLoader.h
@@ -24,7 +24,6 @@ class LevelLoader {
   /**
    * @brief Parse levelFilePath (relative to assets/) and populate level.
    * Clears all existing level data before loading.
-   *  -
    *
    * @param levelFilePath path relative to assets/ dir ("levels/level1.txt")
    * @param level         Level object to populate
